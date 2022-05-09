982119f87ef12f0de097fc07efbb154dd21abcc4
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-09 11:04:57 -0500

    created header and styled it, working on user auth

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+
+function Header() {
+  return ( 
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href='/'>
+            <img className='object-contain w-20 cursor-pointer'
+                 src='https://cdn-icons-png.flaticon.com/512/4697/4697260.png' 
+                 alt=''
+            />
+        </Link>
+        <div className='hidden md:inline-flex items-center space-x-5 font-semibold'>
+            <h3 className='hover:shadow-lg rounded-full px-4 py-1 transition duration-200 ease-out cursor-pointer'>About</h3>
+            <h3 className='hover:shadow-lg rounded-full px-4 py-1 transition duration-200 ease-out cursor-pointer'>Contact</h3>
+            <button className='text-white bg-emerald-200 rounded-full px-4 py-1 cursor-pointer hover:shadow-lg hover:animate-pulse'>Follow</button>
+        </div>
+      </div>
+      
+      <div className='flex items-center space-x-5 text-emerald-800 cursor-pointer transition duration-200 ease-out'>
+        <h3 className='hover:shadow-lg rounded-full px-4 py-1'>Sign In</h3>
+        <h3 className='border px-4 py-1 rounded-full border-emerald-200 hover:border-none hover:text-white hover:bg-emerald-200 transition duration-200 ease-out hover:shadow-lg'>Get Started</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header
