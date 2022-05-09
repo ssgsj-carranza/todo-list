5a7f3a283d36a54fdf17d27c10f482e5768d8042
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-09 12:37:09 -0500

    finished styling sign in page and added redirect to homepage after sign in

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,16 +1,17 @@
 import {getProviders, signIn as SignIntoProvider} from "next-auth/react";
+import Header from "../../components/Header";
 
 function signIn({providers}) {
     return (
        <>
        <Header />
-       <div>
+       <div className="flex flex-col items-center justify-center min-h-screen py-2 px-14 text-center">
            <img className='w-80' src="https://cdn-icons-png.flaticon.com/512/4697/4697260.png" alt=''/>
         <div className="mt-40">
                 {Object.values(providers).map((provider) => (
                     <div key={provider.name}>
-                        <button className="p-3 bg-blue-500 rounded-full text-white"
-                                onClick={() => SignIntoProvider(provider.id)}>
+                        <button className="p-3 bg-blue-500 rounded-full text-white hover:shadow-lg font-semibold"
+                                onClick={() => SignIntoProvider(provider.id, {callbackUrl: '/'})}>
                             Sign in with {provider.name}
                         </button>
                     </div>
