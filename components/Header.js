857c6649159e632eb146a9c18ee4c93e7e3e1306
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-09 13:05:26 -0500

    added and created content body

--- components/Header.js
+++ components/Header.js
@@ -17,16 +17,16 @@ function Header() {
         <div className='hidden md:inline-flex items-center space-x-5 font-semibold'>
             <h3 className='hover:shadow-lg rounded-full px-4 py-1 transition duration-200 ease-out cursor-pointer'>About</h3>
             <h3 className='hover:shadow-lg rounded-full px-4 py-1 transition duration-200 ease-out cursor-pointer'>Contact</h3>
-            <button className='text-white bg-emerald-200 rounded-full px-4 py-1 cursor-pointer hover:shadow-lg hover:animate-pulse'>Follow</button>
+            <button className='text-white bg-indigo-400 rounded-full px-4 py-1 cursor-pointer hover:shadow-lg hover:animate-pulse'>Follow</button>
         </div>
       </div>
       
-      <div className='flex items-center space-x-5 text-emerald-800 cursor-pointer transition duration-200 ease-out'>
+      <div className='flex items-center space-x-5 text-indigo-800 cursor-pointer transition duration-200 ease-out'>
         <button className='hover:shadow-lg rounded-full px-4 py-1 font-semibold'
                 onClick={session ? signOut : signIn}
         >{session ? 'Sign Out' : 'Sign In'}
         </button>
-        <h3 className='border px-4 py-1 rounded-full border-emerald-200 hover:border-none hover:text-white hover:bg-emerald-200 transition duration-200 ease-out hover:shadow-lg font-semibold'>Get Started</h3>
+        <h3 className='border px-4 py-1 rounded-full border-indigo-400 hover:border-none hover:text-white hover:bg-indigo-400 transition duration-200 ease-out hover:shadow-lg font-semibold'>Get Started</h3>
       </div>
     </header>
   );
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      {/* Body content */}
+      <div className='flex justify-between items-center bg-indigo-400 border-y border-black py-10 lg:py-0'>
+        <div className="px-10 space-y-5">
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='overline decoration-black decoration-4'>TO-DO</span> is a place to plan, do, accomplish
+          </h1>
+          <h2 className='italic'>
+            It's easy to create a custome to-do list to cater your needs. Connect with millions of others already DOING. 
+          </h2>
+        </div>
+
+        <img className='hidden md:inline-flex h-32 lg:h-full' 
+             src="https://images.unsplash.com/photo-1642543492457-39a2ce63bb59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" 
+             alt="" 
+        />  
+      </div>
     </div>
   )
 }
