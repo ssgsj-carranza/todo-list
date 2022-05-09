ecb238bcaed0119d22846d9a22a192b3a794ca1e
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-09 11:43:30 -0500

    configured next auth to sign in with google provider

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,27 @@
-function signIn() {
+import {getProviders, signIn as SignIntoProvider} from "next-auth/react";
+
+function signIn({providers}) {
     return (
-        <div>
-            <h1>signin page</h1>
-        </div>
-    )
+       <>
+        {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+                <button onClick={() => SignIntoProvider(provider.id)}>
+                    Sign in with {provider.name}
+                </button>
+            </div>
+        ))}
+       </>
+    );
+}
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        }
+    }
 }
 
 export default signIn;
