729de82a1882d9fed283862e7d0c22f6aca79702
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-09 11:29:16 -0500

    hooke dup authentication working on auth styling page

--- firebase.js
+++ firebase.js
@@ -18,3 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
+const storage = getStorage();
+
+export {app, db, storage};
